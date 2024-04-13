03e093f1f2a430f4b56181e4743179059084a5d6
Serlle Rosales <serlle.rosales96@gmail.com>
2024-04-12 18:05:44 -0600

    added weight in other validation

--- app/models/fruit.rb
+++ app/models/fruit.rb
@@ -1,6 +1,6 @@
 class Fruit < ApplicationRecord
   validates :name, :color, :flavor, :weight, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :weight, :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :name, uniqueness: true
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\z}i,
